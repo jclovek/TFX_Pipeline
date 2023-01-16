435653dbc9c399e354b4b7e123a17c37a36caa7c
ker <keith@keipher.com>
2023-01-16 08:59:15 -0800

    sIQ  pipeline - initial commit

---
+++ mongo-api/mongo_api.py
@@ -0,0 +1,90 @@
+import threading
+from flask import Flask, json, request
+from pymongo import MongoClient, errors
+from kafka import KafkaConsumer
+import sys, logging
+
+app = Flask(__name__)
+
+# kafkaBroker = "localhost:9092"
+kafkaBootstrap = "my-cluster-kafka-bootstrap:9092"
+mongoHost = "" 'mongodb://siq-mongo-srv:27017/'
+
+client = MongoClient(mongoHost)
+# ensembl_db = client['ensembl-genes']
+db = client['genes']
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+logging.info("Starting Mongo API.")
+
+# Kafka consumer
+ensembl_consumer = KafkaConsumer(
+    'ensembl-genes',
+     bootstrap_servers=kafkaBootstrap,
+     value_deserializer=lambda m: json.loads(m.decode('ascii'))
+)
+
+disgenet_consumer = KafkaConsumer(
+    'disgenet-genes',
+     bootstrap_servers=kafkaBootstrap,
+     value_deserializer=lambda m: json.loads(m.decode('ascii'))
+)
+
+def consume_ensembl():
+    for message in ensembl_consumer:
+        data = message.value
+        # ensembl_db.mycollection.insert_one(data)
+        try:
+            logging.info("Sending kafka event to MongoDB - ensembl.")
+            db.ensemblGenes.insert_one(data)
+        except errors.DuplicateKeyError:
+            logging.error(f"Duplicate key error occured while inserting data {data}")
+        except Exception as e:
+            logging.info(f"Error occured while inserting data {data} to MongoDB: {e}")
+
+def consume_disgenet():
+    for message in disgenet_consumer:
+        data = message.value
+        # disgenet_db.mycollection.insert_one(data)
+        try:
+            logging.info("Sending kafka event to MongoDB - disgenet.")
+            db.disgenetGenes.insert_one(data)
+        except errors.DuplicateKeyError:
+            logging.info(f"Duplicate key error occured while inserting data {data}")
+        except Exception as e:
+            logging.info(f"Error occured while inserting data {data} to MongoDB: {e}")
+
+@app.route('/ensembl', methods=['POST'])
+def save_ensembl():
+    data = request.get_json()
+    # ensembl_db.mycollection.insert_one(data)
+    # return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    try:
+        db.ensemblGenes.insert_one(data)
+        logging.info("Saved to MongoDB - ensembl!")
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    except errors.DuplicateKeyError:
+        logging.error(f"Duplicate key error occured while inserting data {data}")
+    except Exception as e:
+        logging.error(f"Error occured while inserting data {data} to MongoDB: {e}")
+
+@app.route('/disgenet', methods=['POST'])
+def save_disgenet():
+    data = request.get_json()
+    # disgenet_db.mycollection.insert_one(data)
+    # return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    print("Saving to MongoDB - disgenet")
+    try:
+        db.disgenetGenes.insert_one(data)
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+        logging.info("Saved to MongoDB - ensembl!")
+    except errors.DuplicateKeyError:
+        print(f"Duplicate key error occured while inserting data {data}")
+    except Exception as e:
+        print(f"Error occured while inserting data {data} to MongoDB: {e}")
+
+if __name__ == '__main__':
+    ensembl_thread = threading.Thread(target=consume_ensembl)
+    ensembl_thread.start()
+    disgenet_thread = threading.Thread(target=consume_disgenet)
+    disgenet_thread.start()
+    app.run(host='0.0.0.0', port=5020, debug=False)
---
+++ postgres/postgres_import_api.py
@@ -0,0 +1,102 @@
+import threading
+import sys, logging
+from flask import Flask, request, json, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemyError
+from kafka import KafkaConsumer
+
+# todo - move to env variables or kube secrets
+sqlHost = "postgres:5432"
+sqlUser = "admin"
+sqlPassword = "changeme"
+
+app = Flask(__name__)
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+logging.info('get_gene called for id: %s', id)
+# 'postgresql://admin:psltest@host:port/database'
+URI = 'postgresql://' + sqlUser + ':' + sqlPassword + '@' + sqlHost + '/postgresdb'
+print("SQLALCHEMY_DATABASE_URI: " + URI, file=sys.stderr)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + sqlUser + ':' + sqlPassword + '@' + sqlHost + '/postgresdb'
+db = SQLAlchemy(app)
+# URI = 'postgresql://' + sqlUser + ':' + sqlPassword + '@' + sqlHost + '/postgres'
+# print("SQLALCHEMY_DATABASE_URI: " | URI, file=sys.stderr)
+
+kafkaBootstrap = "my-cluster-kafka-bootstrap:9092"
+
+ensembl_consumer = KafkaConsumer(
+    'ensembl-genes',
+     bootstrap_servers=kafkaBootstrap,
+     value_deserializer=lambda m: json.loads(m.decode('ascii'))
+)
+
+class Gene(db.Model):
+    __tablename__ = 'genes'
+    id = db.Column(db.String(), primary_key=True)
+    display_name = db.Column(db.String())
+    assembly_name = db.Column(db.String())
+    biotype = db.Column(db.String())
+    object_type = db.Column(db.String())
+    strand = db.Column(db.Integer())
+    db_type = db.Column(db.String())
+    version = db.Column(db.Integer())
+    seq_region_name = db.Column(db.String())
+    source = db.Column(db.String())
+    species = db.Column(db.String())
+    start_pos = db.Column(db.Integer())
+    logic_name = db.Column(db.String())
+    end_pos = db.Column(db.Integer())
+    description = db.Column(db.String())
+    canonical_transcript = db.Column(db.String())
+
+
+def add_gene(data):
+    print("add_gene()", file=sys.stderr)
+    try:
+      new_gene = Gene(id=data['id'], 
+                      display_name=data['display_name'], 
+                      assembly_name=data['assembly_name'], 
+                      biotype=data['biotype'],
+                      object_type=data['object_type'], 
+                      strand=data['strand'], 
+                      db_type=data['db_type'], 
+                      version=data['version'], 
+                      seq_region_name=data['seq_region_name'], 
+                      source=data['source'],
+                      species=data['species'],
+                      start_pos=data['start'],
+                      logic_name=data['logic_name'],
+                      end_pos=data['end'],
+                      description=data['description'],
+                      canonical_transcript=data['canonical_transcript'])
+      db.session.add(new_gene)
+      db.session.commit()
+      logging.info('add_gene() success')
+      return jsonify({'message': 'new gene added successfully'})
+    except SQLAlchemyError as e:
+      error = str(e.__dict__['orig'])
+      logging.error("add_gene() fail - " + error )
+      return jsonify({'message': 'An error occurred while adding the gene'})
+
+def consume_ensembl():
+    logging.info("consume_ensembl()")
+    for message in ensembl_consumer:
+        data = message.value
+        # ensembl_db.mycollection.insert_one(data)
+        add_gene(data)
+
+@app.route('/sqldb', methods=['POST'])
+def send_to_postgres():
+  data = request.get_json()
+  try:
+    add_gene(data)
+    return jsonify({'message': 'new gene added successfully'})
+  except:
+    return jsonify({'message': 'An error occurred while adding the gene'})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5030, debug=True)
+    with app.app_context(): 
+      ensembl_thread = threading.Thread(target=consume_ensembl)
+      ensembl_thread.start()    
+      db.create_all()
